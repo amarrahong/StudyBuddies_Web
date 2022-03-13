80f0babddbcb2550d67aae79d0cfe5050c0b3e1e
Brian Cherng <briancherngsch@gmail.com>
2022-03-12 18:07:34 -0800

    anything for eugene

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
 //load from sooooooooomewhere
-document.getElementById("timeofday-placeholder").innerHTML = "afternoon"
+document.getElementById("timeofday-placeholder").innerHTML = "Afternoon"
 
 //load from someeeeeeeeeeeeewhere
 document.getElementById("username-placeholder").innerHTML = "Simrat"
 
 //load from the abyss 
 document.getElementById("date-placeholder").innerHTML = "3/12/2022"
+
+//load from the
+document.getElementById("time-estimate-placeholder").innerHTML = "1";
+
+//load from
+document.getElementById("streak-placeholder").innerHTML = "3";
