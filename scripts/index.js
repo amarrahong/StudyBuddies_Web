7d7d93aa3c988f7a4b1524db696cf7c1afcb146b
Eugene Jeong <caffeinemousse@gmail.com>
2022-03-12 20:39:06 -0800

    brokey

--- scripts/index.js
+++ scripts/index.js
@@ -17,16 +17,14 @@ class Timer {
     root.innerHTML = Timer.getHTML();
 
     this.el = {
-      hours: root.querySelector("timerpartHours"),
-      minutes: root.querySelector(".timerpartMinutes"),
-      seconds: root.querySelector(".timerpartSeconds"),
-      control: root.querySelector(".timerbtnControl"),
-      reset: root.querySelector(".timerbtnReset")
+      minutes: root.querySelector(".timer__part--minutes"),
+      seconds: root.querySelector(".timer__part--seconds"),
+      control: root.querySelector(".timer__btn--control"),
+      reset: root.querySelector(".timer__btn--reset")
     };
 
     this.interval = null;
     this.remainingSeconds = 0;
-    this.remainingMinutes = 0;
 
     this.el.control.addEventListener("click", () => {
       if (this.interval === null) {
@@ -38,24 +36,19 @@ class Timer {
 
     this.el.reset.addEventListener("click", () => {
       const inputMinutes = prompt("Enter number of minutes:");
+
       if (inputMinutes < 60) {
         this.stop();
         this.remainingSeconds = inputMinutes * 60;
-        this.updateInterfaceTime();
-      } else if (inputMinutes >= 60) {
-        this.stop();
-        this.remainingSeconds = inputMinutes * 60 * 60;
         this.updateInterfaceTime();
       }
     });
   }
 
   updateInterfaceTime() {
-    const hours = this.remainingSeconds;
     const minutes = Math.floor(this.remainingSeconds / 60);
     const seconds = this.remainingSeconds % 60;
 
-    this.el.hours.textContent = hours.toString().padStart(2, "0");
     this.el.minutes.textContent = minutes.toString().padStart(2, "0");
     this.el.seconds.textContent = seconds.toString().padStart(2, "0");
   }
@@ -63,12 +56,12 @@ class Timer {
   updateInterfaceControls() {
     if (this.interval === null) {
       this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
-      this.el.control.classList.add("timerbtnStart");
-      this.el.control.classList.remove("timerbtnStop");
+      this.el.control.classList.add("timer__btn--start");
+      this.el.control.classList.remove("timer__btn--stop");
     } else {
       this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
-      this.el.control.classList.add("timerbtnStop");
-      this.el.control.classList.remove("timerbtnStart");
+      this.el.control.classList.add("timer__btn--stop");
+      this.el.control.classList.remove("timer__btn--start");
     }
   }
 
@@ -97,23 +90,21 @@ class Timer {
 
   static getHTML() {
     return `
-              <span class="timerpart timerpartHours">00</span>
-              <span class="timerpart">:</span>
-              <span class="timerpart timerpartMinutes">00</span>
-              <span class="timerpart">:</span>
-              <span class="timerpart timerpartSeconds">00</span>
-              <button type="button" class="timerbtn timerbtnControl timerbtnStart">
-                  <span class="material-icons">play_arrow</span>
-              </button>
-              <button type="button" class="timerbtn timerbtnReset">
-                  <span class="material-icons">timer</span>
-              </button>
-          `;
+			<span class="timer__part timer__part--minutes">00</span>
+			<span class="timer__part">:</span>
+			<span class="timer__part timer__part--seconds">00</span>
+			<button type="button" class="timer__btn timer__btn--control timer__btn--start">
+				<span class="material-icons">play_arrow</span>
+			</button>
+			<button type="button" class="timer__btn timer__btn--reset">
+				<span class="material-icons">timer</span>
+			</button>
+		`;
   }
 }
 
 new Timer(
-  document.querySelector(".timer")
+	document.querySelector(".timer")
 );
 
 console.log("Is this shit working?")
